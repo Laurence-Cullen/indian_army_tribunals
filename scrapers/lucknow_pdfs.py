08a8c945fca49f2162e0d1e48efe5757044b7f02
Laurence Cullen <laurencesimoncullen@gmail.com>
2023-04-10 22:03:58 +0100

    adding lucknow pdf scraper

--- scrapers/lucknow_pdfs.py
+++ scrapers/lucknow_pdfs.py
@@ -27,17 +27,16 @@ import requests
 #     return s
 
 
-def main():
+def scrape_pdfs(region: str):
     # session = configure_proxies()
 
-    # Load structured_judgements/Chandigarh.csv into DataFrame
-    chandigarh = pd.read_csv('../structured_judgements/chandigarh.csv')
+    region_data = pd.read_csv(f'structured_judgements/{region}.csv')
 
     # Iterate over the DataFrame and download the judgement PDF from each
     # row's 'pdf_url' column
-    for index, row in chandigarh.iterrows():
+    for index, row in region_data.iterrows():
         url = row['pdf_url']
-        filename = f"judgement_pdfs/chandigarh/{row['case_number']}.pdf"
+        filename = f"judgement_pdfs/{region}/{url.split('/')[-1]}.pdf"
 
         # Check if the PDF already exists
         if Path(filename).exists():
@@ -53,7 +52,11 @@ def main():
         time.sleep(random.random() * 0.2)
 
         # Print progress
-        print('Downloaded {} of {} judgements'.format(index + 1, len(chandigarh)))
+        print('Downloaded {} of {} judgements'.format(index + 1, len(region_data)))
+
+
+def main():
+    scrape_pdfs('lucknow')
 
 
 if __name__ == '__main__':
